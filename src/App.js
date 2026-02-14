608f54ba87df141860594c1021798eec75b4b0df
Venkata Sai Chaitanya Varma Sirivuri <venkatasaicha.sirivuri@stonybrook.edu>
2026-02-14 13:42:51 -0500

    fix: migrate Sass @import to @use in App.scss

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Layout from './components/Layout'
 import Home from './components/Home'
 import About from './components/About'
 import './App.scss'
+import 'animate.css'
 import { Route, Routes } from 'react-router-dom'
 import Contact from './components/Contact'
 
