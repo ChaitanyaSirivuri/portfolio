8758f6690016156c33127111e5541edd8271f08c
Venkata Sai Chaitanya Varma Sirivuri <venkatasaicha.sirivuri@stonybrook.edu>
2026-02-17 17:55:58 -0500

    seperate I and , in the homepage

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -28,7 +28,8 @@ const Home = () => {
         <div className="text-zone">
           <h1>
             <span className={letterClass}>H</span>
-            <span className={`${letterClass} _12`}>i,</span>
+            <span className={`${letterClass} _12`}>i</span>
+            <span className={`${letterClass} _12`}>,</span>
             <br />
             <span className={`${letterClass} _13`}>I</span>
             <span className={`${letterClass} _14`}>'m</span>
